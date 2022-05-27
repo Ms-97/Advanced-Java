ae58e18adec61a7ad978bff8d7042f9f5582011c
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-05-27 13:55:07 +0900

    0527(AM)

--- AnnotationTest/src/kr/or/ddit/basic/Service.java
+++ AnnotationTest/src/kr/or/ddit/basic/Service.java
@@ -2,15 +2,15 @@ package kr.or.ddit.basic;
 
 public class Service {
 	
-	@PrintAnnotation()
+	@PrintAnnotation()  // () 생략가능
 	public void method1() {
 		System.out.println("메소드1에서 출력되었습니다.");
 	}
-	@PrintAnnotation(value = "%")
+	@PrintAnnotation(value = "%") // value 생략가능
 	public void method2() {
 		System.out.println("메소드2에서 출력되었습니다.");
 	}
-	@PrintAnnotation(value = "#", count = 25)
+	@PrintAnnotation(value = "#", count = 25) // value 생략 불가능
 	public void method3() {
 		System.out.println("메소드3에서 출력되었습니다.");
 	}
