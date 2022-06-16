5444aefa753789758288f973e7107263a55f3a84
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-06-16 17:30:46 +0900

    0616

---
+++ JavaXMLTest/src/kr/or/ddit/basic/T02DOMParsingTest.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.basic;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class T02DOMParsingTest {
+	public void parse() {
+		
+		// XML문서를 생성하기 위한 DocumentBuilder 객체 생성하기
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = dbf.newDocumentBuilder();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
